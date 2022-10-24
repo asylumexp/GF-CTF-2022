83c5c2bfd8f62037d3c47d39477dde474b3f1de5
DoveC1 <dovec1@shalomcollege.com>
2022-10-24 12:43:03 +1000

    Bot1Test

--- Objects/Red1.py
+++ Objects/Red1.py
@@ -1,23 +1,49 @@
 from GameFrame import RedBot, Globals
 import random
+from enum import Enum
 
+class STATE(Enum):
+    CHILL = 1
+    STRIKE = 2
+    FLAGRETURN = 3
 
 class Red1(RedBot):
     def __init__(self, room, x, y):
         RedBot.__init__(self, room, x, y)
-        self.initial_wait = random.randint(30, 90)
-        self.wait_count = 0
+        self.curr_state=STATE.FLAGRETURN
+
 
     def tick(self):
-        if self.wait_count < self.initial_wait:
-            self.wait_count += 1
+        print(self.curr_state)
+        if self.curr_state == STATE.FLAGRETURN:
+            self.flagreturn()
+        if self.curr_state == STATE.CHILL:
+            self.wait()
+        # if self.curr_state == STATE.STRIKE:
+        #     self.STRIKE()
+
+    def flagreturn(self):
+        if self.point_to_point_distance(self.x, self.y, Globals.blue_flag.x, Globals.blue_flag.y)>20:
+            self.turn_towards(Globals.blue_flag.x,Globals.blue_flag.y)
+            self.drive_forward(Globals.SLOW)
         else:
-            if self.has_flag:
-                self.turn_towards(Globals.SCREEN_WIDTH, self.y)
-                self.drive_forward(Globals.FAST)
-            elif self.rect.right >= Globals.SCREEN_WIDTH / 2:
-                self.turn_towards(self.starting_x - 400, self.starting_y, Globals.FAST)
-                self.drive_forward(Globals.FAST)
-            else:
-                self.turn_towards(Globals.red_flag.x, Globals.red_flag.y, Globals.FAST)
-                self.drive_forward(Globals.FAST)
+            self.curr_state= STATE.CHILL
+
+    def wait(self):
+        bot, distance = self.closest_enemy_to_flag()
+        self.turn_towards(bot.x, bot.y, Globals.FAST)
+
+    # def STRIKE(self):
+
+    def closest_enemy_to_flag(self):
+        closest_bot = Globals.red_bots[0]
+        shortest_distance = self.point_to_point_distance(closest_bot.x, closest_bot.y,
+                                                         Globals.blue_flag.x, Globals.blue_flag.y)
+        for curr_bot in Globals.red_bots:
+            curr_bot_dist = self.point_to_point_distance(curr_bot.x, curr_bot.y,
+                                                         Globals.blue_flag.x, Globals.blue_flag.y)
+            if curr_bot_dist < shortest_distance:
+                shortest_distance = curr_bot_dist
+                closest_bot = curr_bot
+
+        return closest_bot, shortest_distance

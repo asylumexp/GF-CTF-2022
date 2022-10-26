7334c61edc0a96acf1bf0abf64a092e1998c05cc
Sam Heinz <sam@samheinz.com>
2022-10-26 11:02:30 +1000

    System to stop multiple bots targetting one bot in two lines of code

--- Objects/Red1.py
+++ Objects/Red1.py
@@ -14,6 +14,13 @@ class Red1(RedBot):
     def __init__(self, room, x, y):
         RedBot.__init__(self, room, x, y)
         self.curr_state=STATE.FLAGRETURN
+        self.attacking = {
+            "attacking1": [False, None],
+            "attacking2": [False, None],
+            "attacking3": [False, None],
+            "attacking4": [False, None],
+            "attacking5": [False, None]
+        }
 
     def tick(self):
         if self.curr_state == STATE.FLAGRETURN:

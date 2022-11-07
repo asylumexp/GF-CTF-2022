278b238b44d1697eba420e88c558a64ec7858b55
Will Coggins <coggiw1@shalomcollege.com>
2022-11-07 13:24:26 +1000

    adding like htee uhhh like the uhhh blue bots i think? blue 5? perhaps just ,ayber adding red 5 into bot 5, idk ttbh fr fr

--- Objects/Blue5.py
+++ Objects/Blue5.py
@@ -1,23 +1,182 @@
-from GameFrame import BlueBot, Globals
-import random
+from GameFrame import RedBot, Globals
+from enum import Enum
 
 
-class Blue5(BlueBot):
+class STATE(Enum):
+    TIANSHUI = 0 # * Wait state
+    HANDAN = 1 # * Strike state
+    PINQLIANG = 2 # * Move to area state
+    BAO = 3 # * Bait state
+    BAIT_TRUE = 4 # * Prepare bait state
+    EVADE = 5 # * Evade state
+    JAIL = 6 # * Jail state
+    FLAG_ATTACK = 7 # Flag run state
+
+class Red5(RedBot):
     def __init__(self, room, x, y):
-        BlueBot.__init__(self, room, x, y)
-        self.initial_wait = random.randint(30, 90)
-        self.wait_count = 0
+        RedBot.__init__(self, room, x, y)
+        self.set_image("Images/batman2.png", 25, 25)
+        self.curr_state = STATE.TIANSHUI
 
     def tick(self):
-        if self.wait_count < self.initial_wait:
-            self.wait_count += 1
+        # * States
+        print(self.curr_state)
+        if self.curr_state == STATE.PINQLIANG:
+            self.PINQLIANG()
+        elif self.curr_state == STATE.HANDAN:
+            self.HANDAN()
+        elif self.curr_state == STATE.TIANSHUI:
+            self.TIANSHUI()
+        elif self.curr_state == STATE.BAIT_TRUE:
+            self.BAIT_TRUE()
+        elif self.curr_state == STATE.FLAG_ATTACK:
+            self.FLAG_ATTACK()
+        elif self.curr_state == STATE.EVADE:
+            self.EVADE()
+        elif self.curr_state == STATE.BAO:
+            self.BAO()
+        elif self.curr_state == STATE.JAIL:
+            self.JAIL()
         else:
-            if self.has_flag:
-                self.turn_towards(0, self.y)
+            self.curr_state = STATE.TIANSHUI
+
+
+    # * Moving to prepare area
+    def PINQLIANG(self):
+        # * Drive until in position in upper region
+        if self.x <= 644 or self.x >= 656:
+            self.turn_towards(650, 25, Globals.FAST)
+            self.drive_forward(Globals.FAST)
+        # * If that the area, start the bait prepare
+        else:
+            self.curr_state = STATE.BAIT_TRUE
+
+    # * Attack State
+    def HANDAN(self):
+        # * Check for bot
+        bot, distance = self.closest_enemy_to_bot()
+        angle = self.angleRelative(bot.x,bot.y)
+        self.turn_towards(bot.x, bot.y, Globals.SLOW)
+        if distance<100 and angle<70:
                 self.drive_forward(Globals.FAST)
-            elif self.rect.right <= Globals.SCREEN_WIDTH / 2:
-                self.turn_towards(self.starting_x + 400, self.starting_y, Globals.FAST)
+        if distance>100:
+            self.curr_state = STATE.TIANSHUI
+
+    # * Waiting for other bait bots
+    def BAIT_TRUE(self):
+        Globals.red_bots[0].bot5ready = True
+        # if Globals.red_bots[0].bot3ready and Globals.red_bots[0].bot4ready:
+        self.curr_state = STATE.BAO
+
+    #  * Checking for enemies
+    def TIANSHUI(self):
+        bot, distance = self.closest_enemy_to_bot()
+        if distance < 250:
+            self.curr_state = STATE.HANDAN
+
+        else:
+            self.curr_state = STATE.PINQLIANG
+
+    # * Bait state
+    def BAO(self):
+
+        distance_to_flag = self.point_to_point_distance(self.x, self.y, Globals.blue_flag.x, Globals.blue_flag.y)
+        pointX, pointY = self.oppositeDirection()
+        closest_bot, dist = self.closest_enemy_to_bot()
+        self.turn_towards(self.x + pointX, self.y + pointY, Globals.FAST)
+        self.drive_forward(Globals.FAST)
+        if dist > 100:
+            self.curr_state = STATE.FLAG_ATTACK
+        elif dist < 100:
+            if self.y < 100:
+                self.turn_towards(self.x - 100, self.y + 100)
+                self.drive_forward(Globals.FAST)
+            elif self.y > 650:
+                self.turn_towards(self.x - 100, self.y - 100)
                 self.drive_forward(Globals.FAST)
             else:
-                self.turn_towards(Globals.blue_flag.x, Globals.blue_flag.y, Globals.FAST)
+                self.turn_towards(self.x + pointX, self.y + pointY, Globals.FAST)
                 self.drive_forward(Globals.FAST)
+        elif closest_bot < 100:
+            self.turn_towards(self.x + pointX, self.y + pointY, Globals.FAST)
+            self.drive_forward(Globals.FAST)
+
+
+    # flag run
+    def FLAG_ATTACK(self):
+        if self.x >= 1200 and self.y >= 650:
+            self.curr_state = STATE.JAIL
+        bot, distance = self.closest_enemy_to_bot()
+        distance = self.point_to_point_distance(self.x, self.y, bot.x, bot.y)
+        if distance < 50:
+            self.turn_left(Globals.FAST)
+            self.drive_forward(Globals.FAST)
+        elif not self.has_flag:
+            self.turn_towards(Globals.red_flag.x, Globals.red_flag.y, Globals.FAST)
+            self.drive_forward(Globals.FAST)
+        elif self.has_flag:
+            self.turn_towards(Globals.red_bots[0].x, Globals.red_bots[0].y, Globals.FAST)
+            self.drive_forward(Globals.FAST)
+
+    # * Jail state
+    def JAIL(self):
+        # Globals.red_bots[0].bot5ready = False
+        if not self.jailed:
+            self.curr_state = STATE.TIANSHUI
+
+    # * Evade state
+    def EVADE(self):
+        # ! WAITING ON CHARLIE OR SAMS EVADE CODE TO FINISH
+        pass
+
+    # ** Helper Functions **
+
+    def oppositeDirection(self):
+        closest_bot, dist = self.closest_enemy_to_bot()
+        pointX = self.x - closest_bot.x
+        pointY = self.y - closest_bot.y
+        return pointX,pointY
+
+    # * get closest enemy to self
+    def closest_enemy_to_bot(self):
+        closest_bot = Globals.blue_bots[0]
+
+        shortest_distance = self.point_to_point_distance(closest_bot.x, closest_bot.y,
+                                                         Globals.red_bots[4].x, Globals.red_bots[4].y)
+        for curr_bot in Globals.blue_bots:
+
+            curr_bot_dist = self.point_to_point_distance(curr_bot.x, curr_bot.y,
+                                                         Globals.red_bots[4].x, Globals.red_bots[4].y)
+            if curr_bot_dist < shortest_distance:
+                shortest_distance = curr_bot_dist
+                closest_bot = curr_bot
+
+        return closest_bot, shortest_distance
+
+    # * Get closest enemy to the flag
+    def closest_enemy_to_flag(self):
+        closest_bot = Globals.blue_bots[0]
+        shortest_distance = self.point_to_point_distance(closest_bot.x, closest_bot.y,
+                                                         Globals.blue_flag.x, Globals.blue_flag.y)
+        for curr_bot in Globals.blue_bots:
+            curr_bot_dist = self.point_to_point_distance(curr_bot.x, curr_bot.y,
+                                                         Globals.blue_flag.x, Globals.blue_flag.y)
+            if curr_bot_dist < shortest_distance:
+                shortest_distance = curr_bot_dist
+                closest_bot = curr_bot
+
+        return closest_bot, shortest_distance
+
+    # * Relative angle calculation
+    def angleRelative(self,x,y):
+        angle=self.NormalizedAngle(x,y)
+        diffangle=min(abs(self.angle-angle),360-abs(self.angle-angle))
+        return diffangle
+
+    # * normalised angle calculation
+    def NormalizedAngle(self,x,y):
+        angle = self.get_rotation_to_coordinate(x,y)
+        if angle<0: angle+=360
+        return angle
+
+#

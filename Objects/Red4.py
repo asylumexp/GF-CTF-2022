545723a0bc0876a497fdd9d765f5850aeb9232ff
Charlie D <dovec1@shalomcollege.com>
2022-11-08 08:14:02 +1000

    Angle

--- Objects/Red4.py
+++ Objects/Red4.py
@@ -62,7 +62,7 @@ class Red4(RedBot):
         if self.x >= 1200 and self.y >= 650:
             self.curr_state = STATE.JAIL
         # ? move across border, evading enemies
-        elif (angle<80 and distance<250) and not self.has_flag:
+        elif (angle<90 and distance<250) and not self.has_flag:
             self.evadeBots()
         elif not self.has_flag:
             self.turn_towards(Globals.red_flag.x, Globals.red_flag.y, Globals.FAST)
